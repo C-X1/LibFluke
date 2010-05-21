c1ddfa1ece705f68a11898d65d4bccf1089b0984
Christian Holl <cyborgx1@gmail.com>
2010-05-21 10:26:34 +0000

    inital import

---
+++ src/Fluke189.cpp
@@ -0,0 +1,257 @@
+/*
+ * Fluke189.cpp
+ *
+ *  Created on: 06.01.2010
+ *      Author: cyborg-x1
+ */
+
+#include "Fluke189.hpp"
+
+using namespace LibSerial;
+namespace Fluke {
+
+	Fluke189::Fluke189() {
+		SetupInterface();
+	}
+
+	Fluke189::Fluke189(std::string filename){
+		SetupInterface();
+		this->SetDeviceFilename(filename);
+	}
+
+	Fluke189::~Fluke189() {
+		// TODO Auto-generated destructor stub
+	}
+
+	void Fluke189::SetupInterface( void )
+	{
+		//Set Device Communication Parameters
+		//They never change, so lets fix them here.
+			this->SetDeviceBaudRate(SerialPort::BAUD_9600);
+			this->SetDeviceCharacterSize(SerialPort::CHAR_SIZE_8);
+			this->SetDeviceStopBits(SerialPort::STOP_BITS_1);
+			this->SetDeviceFlowControl(SerialPort::FLOW_CONTROL_NONE);
+			this->SetDeviceParity(SerialPort::PARITY_NONE);
+		//Setup optional information
+			this->SetManufacturer("Fluke Corporation");
+			this->SetDeviceName("Fluke 189 Logging Multimeter");
+		//Setup delay to wait for multimeter response
+			this->DelayChar_us=2000;
+			this->DelayResponse_us=120000;
+	}
+	std::vector<Fluke189::SimpleListElement__QD4_BIN__t>Fluke189::CMD_QD4_CreateSimpleList_BIN()
+	{
+		RCT_QD4 rct = this->CMD_QD4(1);
+		std::vector<Fluke189::SimpleListElement__QD4_BIN__t> list;
+		//TODO Add error reporting ... etc....
+		SimpleListElement__QD4_BIN__t currentelement;
+
+		for(unsigned int i=0;i<rct.Data()->DataCount;i++)
+		{
+			//Special Modes
+			currentelement.Auto=rct.Data()->dsets[i].I_S_Auto;
+			currentelement.Hold=rct.Data()->dsets[i].I_S_Hold;
+			currentelement.Log=rct.Data()->dsets[i].I_S_Log;
+
+			//MIN MAX AVG
+			currentelement.Avg=rct.Data()->dsets[i].I_S_Avg;
+			currentelement.Max=rct.Data()->dsets[i].I_S_Max;
+			currentelement.Min=rct.Data()->dsets[i].I_S_Min;
+
+
+			//Errors
+			currentelement.Leads=rct.Data()->dsets[i].I_ErrorNoPV0==LEADS_CONNECTION_WRONG ||
+								 rct.Data()->dsets[i].I_ErrorNoPV1==LEADS_CONNECTION_WRONG;
+
+			currentelement.Fuse=rct.Data()->dsets[i].I_ErrorSV==FUSE;
+			currentelement.Open=rct.Data()->dsets[i].I_ErrorNoPV0==OPEN__NOTHING_CONNECTED ||
+								rct.Data()->dsets[i].I_ErrorNoPV1==OPEN__NOTHING_CONNECTED;
+
+			currentelement.Error=rct.Data()->dsets[i].I_ErrorPV0 ||
+								 rct.Data()->dsets[i].I_ErrorPV1 ||
+								 rct.Data()->dsets[i].I_ErrorSV;
+
+
+			//Low Battery Warning
+			currentelement.LowBat=rct.Data()->dsets[i].I_LowBattery;
+
+			//Range
+			switch(rct.Data()->dsets[i].I_RangeMode)
+			{
+			case RANGE_1: 		currentelement.Range=1; 	break;
+			case RANGE_10: 		currentelement.Range=10;	break;
+			case RANGE_100: 	currentelement.Range=100; 	break;
+			case RANGE_1000: 	currentelement.Range=1000; 	break;
+			case RANGE_5: 		currentelement.Range=5; 	break;
+			case RANGE_50: 		currentelement.Range=50; 	break;
+			case RANGE_500: 	currentelement.Range=500; 	break;
+			case RANGE_5000: 	currentelement.Range=5000; 	break;
+			}
+
+			switch(rct.Data()->dsets[i].I_MeasureMode)
+			{
+			case MM_AC_Ampere:
+				currentelement.PriUnit=SL_AC_Ampere;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_AC_Volt:
+				currentelement.PriUnit=SL_AC_Volts;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_AC_mVolt:
+				currentelement.PriUnit=SL_AC_Volts;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_AC_uAmpere:
+				currentelement.PriUnit=SL_AC_Ampere;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_CAP_Farad:
+				currentelement.PriUnit=SL_Farad;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_DC_A_Submode:
+
+				break;
+			case MM_DC_Ampere:
+				currentelement.PriUnit=SL_DC_Ampere;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_DC_V_Submode:
+
+				break;
+			case MM_DC_Volt:
+				currentelement.PriUnit=SL_DC_Volts;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_DC_mA_Submode:
+
+				break;
+			case MM_DC_mV_Submode:
+
+				break;
+			case MM_DC_mVolt:
+				currentelement.PriUnit=SL_DC_Volts;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_DC_uAmpere:
+				currentelement.PriUnit=SL_DC_Ampere;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_Diode_Volt:
+				currentelement.PriUnit=SL_DC_Volts;//??
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_POS_BETW_2_MODES:
+				currentelement.PriUnit=SL_DISP_OFF;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_R_BEEP_Ohm:
+				currentelement.PriUnit=SL_Ohm;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_R_Ohm:
+				currentelement.PriUnit=SL_Ohm;
+				currentelement.SecUnit=SL_DISP_OFF;
+				break;
+			case MM_Temp_Celsius:
+				currentelement.PriUnit=SL_Celsius;
+				currentelement.SecUnit=SL_Celsius;
+				break;
+			case MM_Temp_Fahrenheit:
+				currentelement.PriUnit=SL_Fahrenheit;
+				currentelement.SecUnit=SL_Fahrenheit;
+				break;
+
+//			default:
+				//TODO Error
+			}
+
+			//Ampere Substates
+			if(rct.Data()->dsets[i].I_MeasureMode==MM_DC_A_Submode ||
+		       rct.Data()->dsets[i].I_MeasureMode==MM_DC_mA_Submode)
+			{
+				switch(rct.Data()->dsets[i].I_SubState_ACDC)
+				{
+				case 0:
+					//TODO Error
+					break;
+				case 1:
+					currentelement.PriUnit=SL_AC_Ampere;
+					currentelement.SecUnit=SL_DC_Ampere;
+					break;
+				case 2:
+					currentelement.PriUnit=SL_DC_Ampere;
+					currentelement.SecUnit=SL_AC_Ampere;
+					break;
+				case 3:
+					currentelement.PriUnit=SL_ACDC_Ampere;
+					currentelement.SecUnit=SL_DISP_OFF;
+					break;
+				}
+			}
+			//Volt Substates
+			if(rct.Data()->dsets[i].I_MeasureMode==MM_DC_V_Submode ||
+			   rct.Data()->dsets[i].I_MeasureMode==MM_DC_mV_Submode)
+			{
+				switch(rct.Data()->dsets[i].I_SubState_ACDC)
+				{
+				case 0:
+					//TODO Error
+					break;
+				case 1:
+					currentelement.PriUnit=SL_AC_Volts;
+					currentelement.SecUnit=SL_DC_Volts;
+					break;
+				case 2:
+					currentelement.PriUnit=SL_DC_Volts;
+					currentelement.SecUnit=SL_AC_Volts;
+					break;
+				case 3:
+					currentelement.PriUnit=SL_ACDC_Volts;
+					currentelement.SecUnit=SL_DISP_OFF;
+					break;
+				}
+			}
+
+
+			if(rct.Data()->dsets[i].I_Delta)
+			{
+				currentelement.Delta=1;
+				currentelement.SecUnit=currentelement.PriUnit;
+			}
+
+			if(rct.Data()->dsets[i].I_DeltaPercent)
+			{
+				currentelement.Delta=1;
+				currentelement.Percent=1;
+				currentelement.PriUnit=SL_Percent;
+				currentelement.SecUnit=currentelement.PriUnit;
+			}
+			if(rct.Data()->dsets[i].I_Unit_AC_dB)
+			{
+				currentelement.PriUnit=SL_DC_Volts;
+				currentelement.SecUnit=SL_dB;
+			}
+
+			if(rct.Data()->dsets[i].I_Unit_AC_dB)
+			{
+				currentelement.PriUnit=SL_dB;
+				currentelement.SecUnit=SL_DC_Volts;
+			}
+
+			//Rising Etch / Falling Etch
+			currentelement.FallingEtch=rct.Data()->dsets[i].I_FallingEtch;
+			currentelement.RisingEtch= rct.Data()->dsets[i].I_RisingEtch;
+
+
+
+			//Add the current element to the vector list
+			list.push_back(currentelement);
+		}
+
+		return list;
+	}
+
+}
+
---
+++ src/Fluke189.hpp
@@ -0,0 +1,527 @@
+/*
+ * Fluke189.h
+ *
+ *  Created on: 06.01.2010
+ *      Author: cyborg-x1
+ */
+
+#ifndef FLUKE189_H_
+#define FLUKE189_H_
+
+
+#include "SerialDevice.hpp"
+#include "SerialResponse.hpp"
+#include <string>
+#include <inttypes.h>
+#include <vector>
+
+
+using namespace LibSerial;
+namespace Fluke {
+
+
+
+class Fluke189: public LibSerial::SerialDevice
+{
+public:
+
+
+
+	// TResponseMask Types / ResponseContainer (RCT_<CMD>) / Functions
+	// Variable Prefix
+	//		I_<variable>=important data
+	//		n_<variable>=none important data (like commas, space and line feeds)
+	//		u_<variable>=data purpose unknown
+	//		m_<variable>=special variable for data extraction with union
+	//		<CR> Carriage Return == /r
+
+	//Get Device Name, Firmware Version, ID Number
+	//Command: ID<CR>
+
+	typedef struct {
+		char I_CMD_ACK;
+		char n_CR0;
+		char I_DeviceName[9];
+		char n_CommaAndSpace0[2];
+		char I_FirmwareVersion[5];
+		char n_Comma0;
+		char I_ID [10];
+		char n_CR1;
+	} cmdr_ID_t;
+
+	typedef  SerialResponse<cmdr_ID_t> RCT_ID;
+
+	RCT_ID CMD_ID(bool TerminatedSubstrings)
+	{
+		RCT_ID Response = this->SendCommand<cmdr_ID_t>("ID\r",this->DelayResponse_us, this->DelayChar_us);
+		if(TerminatedSubstrings)//Insert String Terminators by overwriting unneeded stuff like commas and returns etc...
+		{
+			Response[1]=0;
+			Response[11]=0;
+			Response[18]=0;
+			Response[30]=0;
+		}
+		return Response;
+	}
+
+
+	//Get Setup Informations
+	//Command: QS<CR>
+#pragma pack(push,1)
+	typedef struct{
+		char I_CMD_ACK;
+		char n_CR0;
+		char n_QSHeaderInfo_Comma[3];
+		unsigned int I_LogInterval : 16; // Interval for Logging Mode deciseconds
+		unsigned int I_dBREFmV     : 8;  // dB Reference 1=V 0=mV
+		unsigned int u_unknown0	   : 8;  // TODO:What does that do? (always 00?)
+		unsigned int I_dBREF	   : 16; // dB Reference
+				 int I_TempOffset  : 16; // Temperature Offset
+		unsigned int I_DegC_nDegF  :  8; // High if Celsius is selected, Low for Fahrenheit
+		unsigned int u_unkown1	   :  8; // TODO: Find out what that is used for (always 00?)
+		unsigned int I_BL_Off	   : 16; // Backlite off time, deciseconds
+		unsigned int I_MMTime	   : 32; // Current time of the multimeter in deciseconds from day 0
+		unsigned int I_Power_Off   : 16; // Power off time in minutes
+		unsigned int I_Frequency   :  8; // Frequency Setting 0=50Hz 1=60Hz
+		unsigned int u_unknown2	   :  8; // TODO: Find out what that is used for (always 00?)
+		unsigned int I_4Digits     :  8; // Digits setting (0=5 digits , 1=4 digits)
+		unsigned int u_unknown3	   :  8; // TODO: Find out what that is used for (always 00?)
+		unsigned int I_Beep_On	   :  8; // Beeo setting (0=off, 1=on)
+		unsigned int u_ending1	   : 32; // TODO: Find out what the ending is used for (always 0x 00 90 01 c0 00 00 ??? )
+		unsigned int u_ending2	   : 16; // ^^
+	} cmdr_QS_t;
+#pragma pack(pop)
+
+	typedef SerialResponse<cmdr_QS_t> RCT_QS;
+
+
+	RCT_QS CMD_QS(bool TerminatedSubstrings)
+	{
+		RCT_QS Response = this->SendCommand<cmdr_QS_t>("QS\r",this->DelayResponse_us, this->DelayChar_us);
+		if(TerminatedSubstrings)//Insert String Terminators by overwriting unneeded stuff like commas and returns etc...
+		{
+			Response[1]=0;
+			Response[4]=0;
+		}
+		return Response;
+	}
+
+
+	//Get Current Reading
+	//Command QD 0<CR>
+#pragma pack(push,1)
+	typedef struct{
+		char I_CMD_ACK;
+		char n_CR0;
+		char n_QDHeaderInfo_Comma[3];
+		unsigned int I_Time0		 :32; //Time(1st Occur.)
+		union
+		{
+		  signed int I_priValue0	 :32; //Primary Value	 (1st Occur.)
+		  struct
+		  {
+			unsigned int I_ErrorNoPV0:8;  //8 Bit Error Code (If Error == 1)
+			unsigned int 			 :22; //unneeded bits for Error
+			unsigned int I_ErrorPV0	 :2;  //Error when value == 1 (NoError: is 3 when negative or 0 when positiv)
+		  };
+		};
+		unsigned int I_priDecimal0   :8;  //Pri. Dec. Pnt Loc.(1st Occur.)
+		  signed int I_priSI_Prefix0 :8;  //Pri. SI-Prefix 	 (1st Occur.)
+		union
+		{
+		  signed int I_secValue0     :32; //Sec. Disp. Value
+		  struct
+		  {
+			unsigned int I_ErrorNoSV0:8;  //8 Bit Error Code (If Error == 1)
+			unsigned int 			 :22; //unneeded bits for Error
+			unsigned int I_ErrorSV0 :2;  //Error when value == 1 (NoError: is 3 when negative or 0 when positiv)
+		   };
+		 };
+		unsigned int I_secDecimal	 :8;  //Sec. Disp. Dec. Pnt Locaction
+		  signed int I_secSi_Prefix	 :8;  //Sec. Disp. Prefix TODO: Check if correct
+
+		union
+		{
+		  signed int I_secValue1     :32; //Sec. Disp. Value (2. Occurence)
+		  struct
+		  {
+			unsigned int I_ErrorNoSV1:8;  //8 Bit Error Code (If Error == 1)
+			unsigned int 			 :22; //unneeded bits for Error
+			unsigned int I_ErrorSV1  :2;  //Error when value == 1 (NoError: is 3 when negative or 0 when positiv)
+		   };
+		 };
+		unsigned int I_Time1		 :32; //Time(2nd Occur.)
+		union
+		{
+		  signed int I_priValue1	 :32; //Primary Value	 (2nd Occur.)
+		  struct
+		  {
+			unsigned int I_ErrorNoPV1:8;  //8 Bit Error Code (If Error == 1)
+			unsigned int 			 :22; //unneeded bits for Error
+			unsigned int I_ErrorPV1  :2;  //Error when value == 1 (NoError: is 3 when negative or 0 when positiv)
+		  };
+		};
+		unsigned int I_priDecimal1   :8;  //Pri. Dec. Pnt Loc.(2nd Occur.)
+		  signed int I_priSI_Prefix1 :8;  //Pri. SI-Prefix 	 (2nd Occur.)
+
+
+		bool I_S_Auto		 		 :1;  //High if Auto Hold
+		bool I_S_Hold				 :1;  //High if Hold or Auto Hold
+		bool I_S_Log				 :1;  //High if Log Mode
+		bool I_S_Avg		 		 :1;  //High if MinMax:AVG (also Fast)
+		bool I_S_Max		 		 :1;  //High if MinMax:MAX (also Fast)
+		bool I_S_Min		 		 :1;  //High if MinMax:MIN (also Fast)
+		bool I_S_Fast		 	     :1;  //High if Fast MinMax
+		bool I_LowBattery	 		 :1;  //High if LowBattery is on
+
+		bool I_SetupOrViewMem 		 :1;  //Seems to be one when in Setup or in ViewMem
+		bool I_S_Delta		 		 :1;  //High if Delta is on
+		bool I_S_Percent     		 :1;  //High if Percent is on
+		unsigned int I_MEMclr		 :2;  //In ViewMem its 2 if there are saves or logs, if not its 00, if Clr? is displayed its 1
+		unsigned int I_AutoRange	 :1;  //1 when Auto Range
+		unsigned int I_ManualRange	 :1;  //1 when Manual Range
+		bool u_bit1					 :1;  //TODO Find out what that is for
+
+		unsigned int I_DBREF_Set	 :16; //dBRef Value STD:600
+
+		unsigned int I_MeasureMode	 :6;  //Mode (ACV DCV etc...)
+		unsigned int I_Hz_Percent_ms :2;  //(Hz=1 %=2 ms=3)
+
+		bool I_MinMaxMode	 		 :1;  //High if MinMax Mode(also Fast)
+		bool I_Fast			  		 :1;  //High if Fast
+		bool u_bit2 		 		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+        unsigned int I_MinMaxAvg	 :2;  //(1=Max 2=Min 3=AVG)
+		bool u_bit3			 		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+		bool I_RisingEtch	 		 :1;  //High if rising etch displayed
+		bool I_FallingEtch	 		 :1;  //High if falling etch displayed
+
+		unsigned int I_SubState_ACDC :2;  //Substates of V mV mA uA (DC Modes ONLY)
+		bool u_bit4		 	  		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+		bool I_Hold			   		 :1;  //High if Hold     (Low on AutoHold)
+		bool I_AutoHold		 	 	 :1;  //High if AutoHold (Low on Hold)
+		bool u_bit5			 		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+		bool I_dBRef_V_nm	 		 :1;  //High if V selected for dB_Ref, Low if m selected
+		bool I_DegC_nDegF	 		 :1;  //High if Celsius is selected, Low for Fahrenheit
+
+		bool I_Delta				 :1;  //High if Delta(Low if Delta Percent)
+		bool I_DeltaPercent			 :1;  //High if DeltaPercent(Low if Delta)
+		bool u_bit6					 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+		bool I_Unit_AC_dB			 :1;  //High if(Prim is AC and Sec dB) only V and mV (AC)
+		bool I_Unit_dB_AC			 :1;  //High if(Prim is dB and Sec AC) only V and mV (AC)
+		bool u_bit7					 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+		bool u_bit8					 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+		bool I_nTemperature			 :1;  //Bit is one when NOT in Temperature Measure or Setup or Clr? //TODO Redefine Name
+
+		unsigned int I_RangeMode	 :8;  //Range Mode
+		unsigned int I_CurrentView   :8;  //Displays Data of what is on screen currently 1=Measure, 2=Setup, 22=Setup(Timesetting) ...
+		unsigned int u_byte0         :8;  //TODO:Find out what that is for //always 1
+		unsigned int u_byte1         :8;  //TODO:Find out what that is for //always 0
+	} cmdr_QD0_t;
+#pragma pack(pop)
+
+	typedef SerialResponse<cmdr_QD0_t> RCT_QD0;
+
+
+	RCT_QD0 CMD_QD0(bool TerminatedSubstrings)
+	{
+		RCT_QD0 Response = this->SendCommand<cmdr_QD0_t>("QD 0\r",this->DelayResponse_us, this->DelayChar_us);
+		if(TerminatedSubstrings)//Insert String Terminators by overwriting unneeded stuff like commas and returns etc...
+		{
+			Response[1]=0;
+			Response[4]=0;
+		}
+		return Response;
+	}
+
+	//Get Single Saves
+	//Command QD 4<CR>
+
+#pragma pack(push, 1) //Pragma packing width 1
+	typedef struct
+			{
+				unsigned int I_Time0		 :32; //Time(1st Occur.)
+				union
+				{
+				  signed int I_priValue0	 :32; //Primary Value	 (1st Occur.)
+				  struct
+				  {
+					unsigned int I_ErrorNoPV0:8;  //8 Bit Error Code (If Error == 1)
+					unsigned int 			 :22; //unneeded bits for Error
+					unsigned int I_ErrorPV0	 :2;  //Error when value == 1
+				  };
+				};
+				unsigned int I_priDecimal0   :8;  //Pri. Dec. Pnt Loc.(1st Occur.)
+				  signed int I_priSI_Prefix0 :8;  //Pri. SI-Prefix 	 (1st Occur.)
+				union
+				{
+				  signed int I_secValue		 :32; //Sec. Disp. Value
+				  struct
+				  {
+					unsigned int I_ErrorNoSV :8;  //8 Bit Error Code (If Error == 1)
+					unsigned int 			 :22; //unneeded bits for Error
+					unsigned int I_ErrorSV	 :2;  //Error when value == 1
+				   };
+				 };
+				unsigned int I_secDecimal	 :8;  //Sec. Disp. Dec. Pnt Locaction
+				  signed int I_secSi_Prefix	 :8;  //Sec. Disp. Prefix TODO: Check if correct
+				unsigned int u_unknown0		 :16; //TODO::FIND OUT::Most times 0x0013 sometimes 0x0001 or 0x0002 WTF?
+				unsigned int u_unknown1		 :16; //TODO::Make it change to find its purpose
+				unsigned int I_Time1		 :32; //Time(2nd Occur.)
+				union
+				{
+				  signed int I_priValue1	 :32; //Primary Value	 (2nd Occur.)
+				  struct
+			      {
+				    unsigned int I_ErrorNoPV1:8;  //8 Bit Error Code (If Error == 1)
+					unsigned int 			 :22; //unneeded bits for Error
+					unsigned int I_ErrorPV1	 :2;  //Error when value == 1
+				  };
+				};
+				unsigned int I_priDecimal1	 :8;  //Pri. Dec. Pnt Loc.(2nd Occur.)
+
+				  signed int I_priSI_Prefix1 :8;  //Pri. SI-Prefix 	 (2nd Occur.)
+
+						bool I_S_Auto		 :1;  //High if Auto Hold
+						bool I_S_Hold		 :1;  //High if Hold or Auto Hold
+						bool I_S_Log		 :1;  //High if Log Mode
+						bool I_S_Avg		 :1;  //High if MinMax:AVG (also Fast)
+						bool I_S_Max		 :1;  //High if MinMax:MAX (also Fast)
+						bool I_S_Min		 :1;  //High if MinMax:MIN (also Fast)
+						bool I_S_Fast		 :1;	 //High if Fast MinMax
+						bool I_LowBattery	 :1;	 //High if LowBattery is on
+
+				unsigned int I_RangeMode	 :8;  //Range Mode
+
+				unsigned int I_DBREF_Set	 :16; //dBRef Value STD:600
+
+				unsigned int I_MeasureMode	 :6;  //Mode (ACV DCV etc...)
+				unsigned int I_Hz_Percent_ms :2;  //(Hz=1 %=2 ms=3)
+
+						bool I_MinMaxMode	 :1;  //High if MinMax Mode(also Fast)
+						bool I_Fast			 :1;  //High if Fast
+						bool u_bit6			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+				unsigned int I_MinMaxAvg	 :2;  //(1=Max 2=Min 3=AVG)
+						bool u_bit5			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+						bool I_RisingEtch	 :1;  //High if rising etch displayed
+						bool I_FallingEtch	 :1;  //High if falling etch displayed
+
+				unsigned int I_SubState_ACDC :2;  //Substates of V mV mA uA (DC Modes ONLY)
+						bool u_bit4			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+						bool I_Hold			 :1;  //High if Hold     (Low on AutoHold)
+						bool I_AutoHold		 :1;  //High if AutoHold (Low on Hold)
+						bool u_bit3			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+						bool I_dBRef_V_nm	 :1;  //High if V selected for dB_Ref, Low if m selected
+						bool I_DegC_nDegF	 :1;  //High if Celsius is selected, Low for Fahrenheit
+
+						bool I_Delta		 :1;  //High if Delta(Low if Delta Percent)
+						bool I_DeltaPercent	 :1;  //High if DeltaPercent(Low if Delta)
+						bool u_bit2			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+						bool I_Unit_AC_dB	 :1;  //High if(Prim is AC and Sec dB) only V and mV (AC)
+						bool I_Unit_dB_AC	 :1;  //High if(Prim is dB and Sec AC) only V and mV (AC)
+						bool u_bit1			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+						bool u_bit0			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+						bool I_nTemperature	 :1;  //High if in Temperature Mode  //TODO Redefine Name see usage in QD0
+
+				unsigned int I_SelectedRange :8;  //Range: 5 50 500 5000 1 10 100 1000
+				unsigned int u_unknown2	    :16; //TODO: Find out how to change this always 0x0101<ending>
+				unsigned int I_DataEnding	:8;	 //TODO: Find a way to calculate this ending byte (always the same series)
+			}qd4_set_t;
+
+	typedef struct
+	{
+		char I_CMD_ACK;
+		char n_CR0;
+		char n_QDHeaderInfo_Comma[3];
+		unsigned int DataCount:8;
+		qd4_set_t dsets[1];
+	} cmdr_QD4_t;
+	#pragma pack(pop) //Reset pack pragma
+
+
+	typedef  SerialResponse<cmdr_QD4_t> RCT_QD4;
+
+	RCT_QD4 CMD_QD4(bool TerminatedSubstrings)
+	{
+		RCT_QD4 Response = this->SendCommand<cmdr_QD4_t>("QD 4\r",this->DelayResponse_us, this->DelayChar_us);
+		if(TerminatedSubstrings)//Insert String Terminators by overwriting unneeded stuff like commas and returns etc...
+		{
+			Response[1]=0;
+			Response[4]=0;
+		}
+		return Response;
+	}
+
+
+		//TODO Add Function for generating a simple list... //extra field for prefix or direct calculated to Unit
+		//Time, ValuePri, ValuePriUnit, PrefixPri, ValueSec, ValueSecUnit, PrefixSec
+
+	typedef struct
+	{
+		struct time
+		{
+			unsigned short year;
+			unsigned short month:8;
+			unsigned short day:9;
+			unsigned short hour:9;
+			unsigned short minute:10;
+			unsigned short second:10;
+		};
+		signed int PriValue;
+		signed int PriPrefixPowerFact;
+		char PriUnit;
+		signed int SecValue;
+		signed int SecPrefixPowerFact;
+		char SecUnit;
+		unsigned int Range;
+
+		bool RisingEtch :1;
+		bool FallingEtch:1;
+		bool Percent	:1;
+		bool Delta		:1;
+
+
+		bool Auto		:1;
+		bool Hold		:1;
+		bool Log		:1;
+		bool Max		:1;
+		bool Min		:1;
+		bool Avg		:1;
+		bool LowBat		:1;
+		bool Error		:1;
+		bool Fuse		:1;
+		bool Leads		:1;
+		bool NoConOORan	:1;
+		bool Open		:1;
+		bool StrangeVal :1;
+
+
+	} SimpleListElement__QD4_BIN__t;
+
+
+	std::vector<SimpleListElement__QD4_BIN__t> CMD_QD4_CreateSimpleList_BIN();
+
+
+	//Units for Generated Binary SimpleLists
+	enum SimpleListUnits_BIN
+	{
+		SL_DISP_OFF		=0,
+		SL_AC_Ampere	=1,
+		SL_ACDC_Ampere	=2,
+		SL_AC_Volts		=3,
+		SL_ACDC_Volts	=4,
+		SL_Celsius		=5,
+		SL_dB			=6,
+		SL_Fahrenheit	=7,
+		SL_Farad		=8,
+		SL_DC_Ampere	=9,
+		SL_DC_Volts		=10,
+		SL_Hz			=11,
+		SL_ms			=12,
+		SL_Ohm 			=13,
+		SL_Percent		=14,
+	};
+
+
+
+
+	//Fluke Protocol Field Enums
+	enum CMD_ACK //Command Acknowledge and Error Codes
+	{
+		ERR_ACK   =  '0',
+		ERR_FAIL  =  '1',
+		ERR_NODATA=  '5',
+	};
+
+
+	enum Range
+	{
+		RANGE_1		=0x01,
+		RANGE_10	=0x41,
+		RANGE_100	=0x81,
+		RANGE_1000	=0xc1,
+		RANGE_5		=0x21,
+		RANGE_50	=0x61,
+		RANGE_500	=0xA1,
+		RANGE_5000	=0xE1,
+	};
+
+	enum RangeMode
+	{
+		RANGEMODE_AUTO	=0xA0,
+		RANGEMODE_MANUAL=0xC0,
+		RANGEMODE_NONE	=0x80,
+	};
+
+	enum ValueError
+	{
+		Display_OFFLINE			=0x01,
+		OPEN__NOTHING_CONNECTED	=0x16,
+		FUSE					=0x1B,
+		LEADS_CONNECTION_WRONG	=0x1D,
+		OL_OUTOFRANGE_NOCON		=0x21,
+	};
+
+	enum MeasureMode
+	{
+		MM_POS_BETW_2_MODES	=0x000,
+		MM_AC_Volt 			=0x001,
+		MM_AC_mVolt			=0x002,
+		MM_DC_Volt 			=0x003,
+		MM_DC_mVolt			=0x004,
+		MM_R_Ohm   			=0x009,
+		MM_R_BEEP_Ohm		=0x00b,
+		MM_CAP_Farad		=0x00c,
+		MM_Diode_Volt		=0x00d,
+		MM_Temp_Celsius		=0x01a,
+		MM_Temp_Fahrenheit	=0x01b,
+		MM_AC_Ampere		=0x00e,
+		MM_AC_uAmpere		=0x010,
+		MM_DC_Ampere		=0x011,
+		MM_DC_uAmpere		=0x013,
+		MM_DC_V_Submode		=0x005, //one of three
+		MM_DC_mV_Submode	=0x006,
+		MM_DC_A_Submode		=0x014,
+		MM_DC_mA_Submode	=0x016,
+	};
+
+	enum CurrentView
+	{
+		CV_DisplayingMeasure=0x01,
+		CV_InSetup_noTimeSet=0x02,
+	    CV_InSetup_TimeSet=0x22,
+	    CV_Showing_CLR_Saves=0x03,
+	    CV_Showing_CLR_Log=0x05,
+	    CV_ViewMem_NoData=0x06,
+	};
+
+
+
+private:
+	//Private Attributes
+	unsigned int DelayChar_us;
+	unsigned int DelayResponse_us;
+
+
+
+
+public:
+
+	Fluke189();
+	Fluke189(std::string filename);
+	virtual ~Fluke189();
+
+	//Fluke189 Specific Command Functions
+
+
+
+
+
+private:
+	void SetupInterface( void );
+
+
+};
+
+}
+
+#endif /* FLUKE189_H_ */
