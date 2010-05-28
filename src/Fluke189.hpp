22400c8450bd81aa97c0646cf26bd72d870f5ff5
Christian Holl <cyborgx1@gmail.com>
2010-05-28 18:28:48 +0000

    combined all information bits of the qd modes in one block. the same block for each mode now.

--- src/Fluke189.hpp
+++ src/Fluke189.hpp
@@ -32,7 +32,6 @@ public:
 	//		I_<variable>=important data
 	//		n_<variable>=none important data (like commas, space and line feeds)
 	//		u_<variable>=data purpose unknown
-	//		m_<variable>=special variable for data extraction with union
 	//		<CR> Carriage Return == /r
 
 	//Get Device Name, Firmware Version, ID Number
@@ -108,6 +107,64 @@ public:
 	}
 
 
+#pragma pack(push,1)
+	//QD Status Field
+	typedef struct
+	{
+				bool I_S_Auto		 		 :1;  //High if Auto Hold
+				bool I_S_Hold				 :1;  //High if Hold or Auto Hold
+				bool I_S_Log				 :1;  //High if Log Mode
+				bool I_S_Avg		 		 :1;  //High if MinMax:AVG (also Fast)
+				bool I_S_Max		 		 :1;  //High if MinMax:MAX (also Fast)
+				bool I_S_Min		 		 :1;  //High if MinMax:MIN (also Fast)
+				bool I_S_Fast		 	     :1;  //High if Fast MinMax
+				bool I_LowBattery	 		 :1;  //High if LowBattery is on
+
+				bool I_SetupOrViewMem 		 :1;  //Seems to be one when in Setup or in ViewMem
+				bool I_S_Delta		 		 :1;  //High if Delta is on
+				bool I_S_Percent     		 :1;  //High if Percent is on
+		unsigned int I_MEMclr		 		 :2;  //In ViewMem its 2 if there are saves or logs, if not its 00, if Clr? is displayed its 1
+		unsigned int I_AutoRange	 		 :1;  //1 when Auto Range
+		unsigned int I_ManualRange			 :1;  //1 when Manual Range
+				bool u_bit1 				 :1;  //TODO Find out what that is for
+
+		unsigned int I_DBREF_Set	 		 :16; //dBRef Value STD:600
+
+		unsigned int I_MeasureMode	 		 :6;  //Mode (ACV DCV etc...)
+		unsigned int I_Hz_Percent_ms 		 :2;  //(Hz=1 %=2 ms=3)
+
+				bool I_MinMaxMode	 		 :1;  //High if MinMax Mode(also Fast)
+				bool I_Fast			  		 :1;  //High if Fast
+				bool u_bit2 		 		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+		unsigned int I_MinMaxAvg	 		 :2;  //(1=Max 2=Min 3=AVG)
+				bool u_bit3			 		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+				bool I_RisingEtch	 		 :1;  //High if rising etch displayed
+				bool I_FallingEtch	 		 :1;  //High if falling etch displayed
+
+		unsigned int I_SubState_ACDC 		 :2;  //Substates of V mV mA uA (DC Modes ONLY)
+				bool u_bit4		 	  		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+				bool I_Hold			   		 :1;  //High if Hold     (Low on AutoHold)
+				bool I_AutoHold		 	 	 :1;  //High if AutoHold (Low on Hold)
+				bool u_bit5			 		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+				bool I_dBRef_V_nm	 		 :1;  //High if V selected for dB_Ref, Low if m selected
+				bool I_DegC_nDegF	 		 :1;  //High if Celsius is selected, Low for Fahrenheit
+
+				bool I_Delta				 :1;  //High if Delta(Low if Delta Percent)
+				bool I_DeltaPercent			 :1;  //High if DeltaPercent(Low if Delta)
+				bool u_bit6					 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+				bool I_Unit_AC_dB			 :1;  //High if(Prim is AC and Sec dB) only V and mV (AC)
+				bool I_Unit_dB_AC			 :1;  //High if(Prim is dB and Sec AC) only V and mV (AC)
+				bool u_bit7					 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+				bool u_bit8					 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
+				bool I_nTemperature			 :1;  //Bit is one when NOT in Temperature Measure or Setup or Clr? //TODO Redefine Name
+
+		unsigned int I_SelectedRange 	 	 :8;  //Range Mode
+		unsigned int I_CurrentView   		 :8;  //Displays Data of what is on screen currently 1=Measure, 2=Setup, 22=Setup(Timesetting) ...
+	} qdInfo_t;
+#pragma pack(pop)
+
+
+
 	//Get Current Reading
 	//Command QD 0<CR>
 #pragma pack(push,1)
@@ -167,56 +224,8 @@ public:
 		unsigned int I_priDecimal1  		 :8;  //Pri. Dec. Pnt Loc.(2nd Occur.)
 		  signed int I_priSI_Prefix1 		 :8;  //Pri. SI-Prefix 	 (2nd Occur.)
 
+		  qdInfo_t I_QDInfo;
 
-			    bool I_S_Auto		 		 :1;  //High if Auto Hold
-			    bool I_S_Hold				 :1;  //High if Hold or Auto Hold
-			    bool I_S_Log				 :1;  //High if Log Mode
-			    bool I_S_Avg		 		 :1;  //High if MinMax:AVG (also Fast)
-			    bool I_S_Max		 		 :1;  //High if MinMax:MAX (also Fast)
-			    bool I_S_Min		 		 :1;  //High if MinMax:MIN (also Fast)
-			    bool I_S_Fast		 	     :1;  //High if Fast MinMax
-			    bool I_LowBattery	 		 :1;  //High if LowBattery is on
-
-			    bool I_SetupOrViewMem 		 :1;  //Seems to be one when in Setup or in ViewMem
-			    bool I_S_Delta		 		 :1;  //High if Delta is on
-				bool I_S_Percent     		 :1;  //High if Percent is on
-		unsigned int I_MEMclr		 		 :2;  //In ViewMem its 2 if there are saves or logs, if not its 00, if Clr? is displayed its 1
-		unsigned int I_AutoRange	 		 :1;  //1 when Auto Range
-		unsigned int I_ManualRange			 :1;  //1 when Manual Range
-			    bool u_bit1 				 :1;  //TODO Find out what that is for
-
-		unsigned int I_DBREF_Set	 		 :16; //dBRef Value STD:600
-
-		unsigned int I_MeasureMode	 		 :6;  //Mode (ACV DCV etc...)
-		unsigned int I_Hz_Percent_ms 		 :2;  //(Hz=1 %=2 ms=3)
-
-				bool I_MinMaxMode	 		 :1;  //High if MinMax Mode(also Fast)
-				bool I_Fast			  		 :1;  //High if Fast
-				bool u_bit2 		 		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-        unsigned int I_MinMaxAvg	 		 :2;  //(1=Max 2=Min 3=AVG)
-				bool u_bit3			 		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool I_RisingEtch	 		 :1;  //High if rising etch displayed
-				bool I_FallingEtch	 		 :1;  //High if falling etch displayed
-
-		unsigned int I_SubState_ACDC 		 :2;  //Substates of V mV mA uA (DC Modes ONLY)
-				bool u_bit4		 	  		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool I_Hold			   		 :1;  //High if Hold     (Low on AutoHold)
-				bool I_AutoHold		 	 	 :1;  //High if AutoHold (Low on Hold)
-				bool u_bit5			 		 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool I_dBRef_V_nm	 		 :1;  //High if V selected for dB_Ref, Low if m selected
-				bool I_DegC_nDegF	 		 :1;  //High if Celsius is selected, Low for Fahrenheit
-
-				bool I_Delta				 :1;  //High if Delta(Low if Delta Percent)
-				bool I_DeltaPercent			 :1;  //High if DeltaPercent(Low if Delta)
-				bool u_bit6					 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool I_Unit_AC_dB			 :1;  //High if(Prim is AC and Sec dB) only V and mV (AC)
-				bool I_Unit_dB_AC			 :1;  //High if(Prim is dB and Sec AC) only V and mV (AC)
-				bool u_bit7					 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool u_bit8					 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool I_nTemperature			 :1;  //Bit is one when NOT in Temperature Measure or Setup or Clr? //TODO Redefine Name
-
-		unsigned int I_SelectedRange 	 	 :8;  //Range Mode
-		unsigned int I_CurrentView   		 :8;  //Displays Data of what is on screen currently 1=Measure, 2=Setup, 22=Setup(Timesetting) ...
 		unsigned int u_byte0         		 :8;  //TODO:Find out what that is for //always 1
 		unsigned int u_byte1         		 :8;  //TODO:Find out what that is for //always 0
 	} cmdr_QD0_t;
@@ -273,51 +282,7 @@ public:
 		unsigned int I_InitDecimal	 	 :8;  //Initial Disp. Dec. Pnt Locaction
 		  signed int I_InitPrefix    	 :8;  //Initial SI-Prefix
 
-			    bool I_S_Auto		 	 :1;  //High if Auto Hold
-				bool I_S_Hold		 	 :1;  //High if Hold or Auto Hold
-		        bool I_S_Log		 	 :1;  //High if Log Mode
-		        bool I_S_Avg		 	 :1;  //High if MinMax:AVG (also Fast)
-		        bool I_S_Max		 	 :1;  //High if MinMax:MAX (also Fast)
-		        bool I_S_Min		 	 :1;  //High if MinMax:MIN (also Fast)
-		        bool I_S_Fast		 	 :1;	 //High if Fast MinMax
-		        bool I_LowBattery	 	 :1;	 //High if LowBattery is on
-
-		        bool I_SetupOrViewMem	 :1;  //Seems to be one when in Setup or in ViewMem //Unknown if its available in Log Mode
-		        bool I_S_Delta		 	 :1;  //High if Delta is on
-		        bool I_S_Percent     	 :1;  //High if Percent is on
-		unsigned int I_MEMclr		  	 :2;  //In ViewMem its 2 if there are saves or logs, if not its 00, if Clr? is displayed its 1 //Unknown if its available in Log Mode
-		unsigned int I_AutoRange	 	 :1;  //1 when Auto Range
-		unsigned int I_ManualRange	 	 :1;  //1 when Manual Range
-	 	        bool u_bit1			 	 :1;  //TODO Find out what that is for
-
-		unsigned int I_DBREF_Set	     :16; //dBRef Value STD:600
-		unsigned int I_MeasureMode	 	 :6;  //Mode (ACV DCV etc...)
-		unsigned int I_Hz_Percent_ms 	 :2;  //(Hz=1 %=2 ms=3)
-
-				bool I_MinMaxMode		 :1;  //High if MinMax Mode(also Fast)
-				bool I_Fast				 :1;  //High if Fast
-				bool u_bit2				 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-		unsigned int I_MinMaxAvg		 :2;  //(1=Max 2=Min 3=AVG)
-				bool u_bit3				 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool I_RisingEtch	 	 :1;  //High if rising etch displayed
-				bool I_FallingEtch	 	 :1;  //High if falling etch displayed
-
-		unsigned int I_SubState_ACDC     :2;  //Substates of V mV mA uA (DC Modes ONLY)
-				bool u_bit4		 	  	 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool I_Hold			   	 :1;  //High if Hold     (Low on AutoHold)
-				bool I_AutoHold		 	 :1;  //High if AutoHold (Low on Hold)
-				bool u_bit5			 	 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool I_dBRef_V_nm	 	 :1;  //High if V selected for dB_Ref, Low if m selected
-				bool I_DegC_nDegF	 	 :1;  //High if Celsius is selected, Low for Fahrenheit
-
-				bool I_Delta			 :1;  //High if Delta(Low if Delta Percent)
-				bool I_DeltaPercent		 :1;  //High if DeltaPercent(Low if Delta)
-				bool u_bit6				 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool I_Unit_AC_dB		 :1;  //High if(Prim is AC and Sec dB) only V and mV (AC)
-				bool I_Unit_dB_AC		 :1;  //High if(Prim is dB and Sec AC) only V and mV (AC)
-				bool u_bit7				 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool u_bit8				 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				bool I_TODO	 			 :1;  //TODO Redefine Name //Maybe 1 when Decimal Loc is not in use ???
+		  qdInfo_t I_QDInfo;
 
 		unsigned int I_SelectedRange 	 :8;  //Range: 5 50 500 5000 1 10 100 1000
 
@@ -404,49 +369,9 @@ public:
 
 				  signed int I_priSI_Prefix1 :8;  //Pri. SI-Prefix 	 (2nd Occur.)
 
-						bool I_S_Auto		 :1;  //High if Auto Hold
-						bool I_S_Hold		 :1;  //High if Hold or Auto Hold
-						bool I_S_Log		 :1;  //High if Log Mode
-						bool I_S_Avg		 :1;  //High if MinMax:AVG (also Fast)
-						bool I_S_Max		 :1;  //High if MinMax:MAX (also Fast)
-						bool I_S_Min		 :1;  //High if MinMax:MIN (also Fast)
-						bool I_S_Fast		 :1;	 //High if Fast MinMax
-						bool I_LowBattery	 :1;	 //High if LowBattery is on
+				  qdInfo_t I_QDInfo;
 
-				unsigned int I_RangeMode	 :8;  //Range Mode
-
-				unsigned int I_DBREF_Set	 :16; //dBRef Value STD:600
-
-				unsigned int I_MeasureMode	 :6;  //Mode (ACV DCV etc...)
-				unsigned int I_Hz_Percent_ms :2;  //(Hz=1 %=2 ms=3)
-
-						bool I_MinMaxMode	 :1;  //High if MinMax Mode(also Fast)
-						bool I_Fast			 :1;  //High if Fast
-						bool u_bit6			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-				unsigned int I_MinMaxAvg	 :2;  //(1=Max 2=Min 3=AVG)
-						bool u_bit5			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-						bool I_RisingEtch	 :1;  //High if rising etch displayed
-						bool I_FallingEtch	 :1;  //High if falling etch displayed
-
-				unsigned int I_SubState_ACDC :2;  //Substates of V mV mA uA (DC Modes ONLY)
-						bool u_bit4			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-						bool I_Hold			 :1;  //High if Hold     (Low on AutoHold)
-						bool I_AutoHold		 :1;  //High if AutoHold (Low on Hold)
-						bool u_bit3			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-						bool I_dBRef_V_nm	 :1;  //High if V selected for dB_Ref, Low if m selected
-						bool I_DegC_nDegF	 :1;  //High if Celsius is selected, Low for Fahrenheit
-
-						bool I_Delta		 :1;  //High if Delta(Low if Delta Percent)
-						bool I_DeltaPercent	 :1;  //High if DeltaPercent(Low if Delta)
-						bool u_bit2			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-						bool I_Unit_AC_dB	 :1;  //High if(Prim is AC and Sec dB) only V and mV (AC)
-						bool I_Unit_dB_AC	 :1;  //High if(Prim is dB and Sec AC) only V and mV (AC)
-						bool u_bit1			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-						bool u_bit0			 :1;  //TODO: Find a use for this: always 0 (Maybe Cal?)
-						bool I_TODO	 		 :1;  //TODO Redefine Name see usage in QD0 //use Decimal???
-
-				unsigned int I_SelectedRange :8;  //Range: 5 50 500 5000 1 10 100 1000
-				unsigned int u_unknown2	    :16; //TODO: Find out how to change this always 0x0101<ending>
+				unsigned int u_unknown2	    :8; //TODO: Find out how to change this always 0x01<ending>
 				unsigned int I_DataEnding	:8;	 //TODO: Find a way to calculate this ending byte (always the same series)
 			}qd4_set_t;
 
@@ -478,81 +403,12 @@ public:
 		//TODO Add Function for generating a simple list... //extra field for prefix or direct calculated to Unit
 		//Time, ValuePri, ValuePriUnit, PrefixPri, ValueSec, ValueSecUnit, PrefixSec
 
-	typedef struct
-	{
-		struct time
-		{
-			unsigned short year;
-			unsigned short month:8;
-			unsigned short day:9;
-			unsigned short hour:9;
-			unsigned short minute:10;
-			unsigned short second:10;
-		};
-		signed int PriValue;
-		signed int PriPrefixPowerFact;
-		char PriUnit;
-		signed int SecValue;
-		signed int SecPrefixPowerFact;
-		char SecUnit;
-		unsigned int Range;
-
-		bool RisingEtch :1;
-		bool FallingEtch:1;
-		bool Percent	:1;
-		bool Delta		:1;
-
-
-		bool Auto		:1;
-		bool Hold		:1;
-		bool Log		:1;
-		bool Max		:1;
-		bool Min		:1;
-		bool Avg		:1;
-		bool LowBat		:1;
-		bool Error		:1;
-		bool Fuse		:1;
-		bool Leads		:1;
-		bool NoConOORan	:1;
-		bool Open		:1;
-		bool StrangeVal :1;
-
-
-	} SimpleListElement__QD4_BIN__t;
-
-
-	std::vector<SimpleListElement__QD4_BIN__t> CMD_QD4_CreateSimpleList_BIN();
-
-
-	//Units for Generated Binary SimpleLists
-	enum SimpleListUnits_BIN
-	{
-		SL_DISP_OFF		=0,
-		SL_AC_Ampere	=1,
-		SL_ACDC_Ampere	=2,
-		SL_AC_Volts		=3,
-		SL_ACDC_Volts	=4,
-		SL_Celsius		=5,
-		SL_dB			=6,
-		SL_Fahrenheit	=7,
-		SL_Farad		=8,
-		SL_DC_Ampere	=9,
-		SL_DC_Volts		=10,
-		SL_Hz			=11,
-		SL_ms			=12,
-		SL_Ohm 			=13,
-		SL_Percent		=14,
-	};
-
-
-
-
 	//Fluke Protocol Field Enums
 	enum CMD_ACK //Command Acknowledge and Error Codes
 	{
-		ERR_ACK   =  '0',
-		ERR_FAIL  =  '1',
-		ERR_NODATA=  '5',
+		CA_ERR_ACK   =  '0',
+		CA_ERR_FAIL  =  '1',
+		CA_ERR_NODATA=  '5',
 	};
 
 
@@ -577,11 +433,11 @@ public:
 
 	enum ValueError
 	{
-		Display_OFFLINE			=0x01,
-		OPEN__NOTHING_CONNECTED	=0x16,
-		FUSE					=0x1B,
-		LEADS_CONNECTION_WRONG	=0x1D,
-		OL_OUTOFRANGE_NOCON		=0x21,
+		VE_Display_OFFLINE			=0x01,
+		VE_OPEN__NOTHING_CONNECTED	=0x16,
+		VE_FUSE						=0x1B,
+		VE_LEADS_CONNECTION_WRONG	=0x1D,
+		VE_OL_OUTOFRANGE_NOCON		=0x21,
 	};
 
 	enum MeasureMode
